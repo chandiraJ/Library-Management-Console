b02d0d954c520689add7990dbd35234ada1ff364
Chandira Jayasekara <chandirajayasekara@gmail.com>
2020-02-21 19:57:09 +0530

    Before proceeding with the role Librarian

--- server.js
+++ server.js
@@ -5,26 +5,24 @@ const BodyParser = require("body-parser");
 // Imports routes collection from ./routes.
 const Routes = require("./routes");
 
+const app = Express();
 
-
-const app =  Express();
-
-// const Routes = require("");
-
+// Extract the entire body of the request. Makes it easy to access request details.
 app.use(BodyParser.json());
 
+// Executes the imported module when a request hits the specified URL
 app.use('/library', Routes);
 
 // Creates a server listening on the port 8080
 app.listen(8080, 'localhost', function (err) {
-    if(err) {
+    if (err) {
         console.log(err);
         process.exit(1);
     }
     console.log("Server listening port 8080");
 })
 
-//Creates the connection to the database
+//Creates the connection to the Mongo database
 mongoose.connect('mongodb://libuser:libadmin123@ds029831.mlab.com:29831/librarydb', function (err) {
     if (err) {
         console.log(err);
